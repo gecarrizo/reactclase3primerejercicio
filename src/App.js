407f00b21a981d21aa14fa7191e83b24e537c720
“Gabriel <gecarrizo@gmail.com>
2022-01-19 17:45:30 -0300

    Clase 3 React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+let pais = "ARGENTINA";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='App'>HOLA MUNDO!!</h1>
+      <hr/>
+      <p></p>Sommos de {pais}!!! Uso de FRAGMENT ABREVIADO<p/>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <img src='https://www.infobae.com/pf/resources/images/logo_infobae_naranja.svg?d=864' alt='Infobae'></img>
+    </>
   );
 }
 
